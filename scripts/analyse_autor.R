78f72ae44389820a76492f9c2f3083d88a569793
Miriam Stiewe <nixe.st@mail.de>
2026-03-18 10:19:05 +0100

    Add files via upload

---
+++ scripts/analyse_autor.R
@@ -0,0 +1,10 @@
+# Stylo starten
+library(stylo)
+
+# Arbeitsverzeichnis festlegen
+arbeitsverzeichnis <- "../data/corpus_autoren"
+
+# grafische Nutzeroberfläche starten, auf Autorenkorpus anwenden
+stylo(corpus.dir = arbeitsverzeichnis)
+
+# Ergebnisse im Ordner results/Autoren
---
+++ scripts/analyse_medium.R
@@ -0,0 +1,10 @@
+# Stylo starten
+library(stylo)
+
+# Arbeitsverzeichnis festlegen
+arbeitsverzeichnis <- "../data/corpus_alle"
+
+# grafische Nutzeroberfläche starten, auf Gesamtkorpus anwenden
+stylo(corpus.dir = arbeitsverzeichnis)
+
+# Ergebnisse im Ordner results
